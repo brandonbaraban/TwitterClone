eb245683c5bd7d5e2f2c548d75db97b20c1be590
Brandon Baraban <bbaraban@fb.com>
2017-06-27 15:23:58 -0700

    Add ability to refresh tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,6 +20,7 @@ import org.json.JSONObject;
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,6 +36,7 @@ public class TimelineActivity extends AppCompatActivity {
     @BindView(R.id.rvTweet) RecyclerView rvTweets;
     @BindView(R.id.tbMenuTimeline)
     Toolbar tbMenuTimeline;
+    @BindView(R.id.scRefresh) SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,21 @@ public class TimelineActivity extends AppCompatActivity {
 
         // bind views
         ButterKnife.bind(this);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
         // set the toolbar to act as the action bar for this activity
         setSupportActionBar(tbMenuTimeline);
         // init the arraylist (data source)
@@ -57,6 +75,44 @@ public class TimelineActivity extends AppCompatActivity {
 
         populateTimeline();
     }
+
+    public void fetchTimelineAsync() {
+        // Send the network request to fetch the updated data
+        // `client` here is an instance of Android Async HTTP
+        // getHomeTimeline is an example endpoint.
+
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                tweetAdapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                tweetAdapter.addAll(processJSONArrayTweets(response));
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,6 +138,24 @@ public class TimelineActivity extends AppCompatActivity {
         }
     }
 
+    private List<Tweet> processJSONArrayTweets(JSONArray response) {
+        ArrayList<Tweet> newTweets = new ArrayList<>();
+        // iterate through JSONArray
+        // for each entry, deserialize the JSON object
+        for (int i = 0; i < response.length(); i++) {
+            // convert each object to a Tweet model
+            // add that Tweet model to our data source
+            // notify the adapter that we've added an item
+            try {
+                Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                newTweets.add(tweet);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return newTweets;
+    }
+
     private void populateTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
@@ -92,20 +166,7 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 // Log.d("TwitterClient", response.toString());
-                // iterate through JSONArray
-                // for each entry, deserialize the JSON object
-                for (int i = 0; i < response.length(); i++) {
-                    // convert each object to a Tweet model
-                    // add that Tweet model to our data source
-                    // notify the adapter that we've added an item
-                    try {
-                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
-                        tweets.add(tweet);
-                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
+                tweetAdapter.addAll(processJSONArrayTweets(response));
             }
 
             @Override
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -75,4 +75,16 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
             ButterKnife.bind(this, itemView);
         }
     }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        mTweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        mTweets.addAll(list);
+        notifyDataSetChanged();
+    }
 }
