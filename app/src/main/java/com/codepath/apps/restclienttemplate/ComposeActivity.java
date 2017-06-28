98628ecb5d148abea21e6734b664d11b334d9df2
Brandon Baraban <bbaraban@fb.com>
2017-06-28 14:13:17 -0700

    Add Toolbar to ComposeActivity

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,9 +2,14 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -24,9 +29,13 @@ public class ComposeActivity extends AppCompatActivity {
 
     private TwitterClient client;
 
+    private MenuItem miActionProgress;
+
     @BindView(R.id.etStatus) EditText etStatus;
     @BindView(R.id.tvCharCount)
     TextView tvCharCount;
+    @BindView(R.id.tbMenuCompose)
+    Toolbar tbMenuCompose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +46,36 @@ public class ComposeActivity extends AppCompatActivity {
         // bind views
         ButterKnife.bind(this);
 
+        setSupportActionBar(tbMenuCompose);
+        getSupportActionBar().setTitle("Compose tweet");
         tvCharCount.setText("140");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_compose, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Store instance of the menu item containing progress
+        miActionProgress = menu.findItem(R.id.miActionProgress);
+        // Extract the action-view from the menu item
+        ProgressBar v = (ProgressBar) MenuItemCompat.getActionView(miActionProgress);
+        // Return to finish
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    public void showProgressBar() {
+        // Show progress item
+        miActionProgress.setVisible(true);
+    }
+
+    public void hideProgressBar() {
+        // Hide progress item
+        miActionProgress.setVisible(false);
     }
 
     @OnTextChanged(value = R.id.etStatus,
@@ -50,6 +88,9 @@ public class ComposeActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnTweet)
     public void sendTweet() {
+        if (miActionProgress != null) {
+            showProgressBar();
+        }
         client.sendTweet(etStatus.getText().toString(), new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -58,9 +99,15 @@ public class ComposeActivity extends AppCompatActivity {
                     Intent i = new Intent();
                     i.putExtra("tweet", Parcels.wrap(tweet));
                     setResult(RESULT_OK, i);
+                    if (miActionProgress != null) {
+                        hideProgressBar();
+                    }
                     finish();
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    if (miActionProgress != null) {
+                        hideProgressBar();
+                    }
                 }
             }
 
@@ -68,6 +115,9 @@ public class ComposeActivity extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 Log.d("TwitterClient", responseString);
                 throwable.printStackTrace();
+                if (miActionProgress != null) {
+                    hideProgressBar();
+                }
             }
         });
     }
